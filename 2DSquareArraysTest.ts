a86458f3bece40315dd821d3562311042d803145
Ma6irinata <i.vanev@abv.bg>
2018-02-11 12:04:31 +0200

    Adding TS tests

---
+++ 2DSquareArraysTest.ts
@@ -0,0 +1,24 @@
+//test to see how two-dimensional arrays in JS can be appended
+
+class SquareA {
+    value: number;
+    constructor(val:number) {
+        if (val >= 0) this.value = val;
+    }
+}
+
+class BoardA {
+    rows: number;
+    columns: number;
+    state: SquareA[][]; // other def: SquareA[][];
+    constructor(rowz: number,cols: number){
+        this.rows = rowz;
+        this.columns = cols;
+        for (let i = 0; i <= this.rows; i++)
+            for (let j = 0; j <= this.columns; j++)
+                this.state[i].push(new SquareA(0));
+    }
+}
+
+var board = new BoardA(2,2);
+console.log(board);
